f1501b99ea83a8fa999e2121a90c2e9f3f9dd065
m-nagacharan <hello@nagacharan.me>
2021-04-13 20:45:11 +0200

    Initial commit

--- GraphQueries.hs
+++ GraphQueries.hs
@@ -1,15 +1,47 @@
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE MonadComprehensions #-}
+{-# LANGUAGE RankNTypes #-}
+import Prelude hiding (id)
 data Node a = Node {
     id :: a,
-    adjacent :: [Node a]
-}
+    adjacent :: [a]
+} deriving Show
 
+{- instance Show (Node a) where 
+    show (Node x y) = " -- " ++ show x ++ " -- "
+ -}
 data Graph a = Graph [Node a]
 
 data AILabel a = AILabel {
+    id  :: a,
     pre :: Int,
     post :: Int,
-    hops :: [Node a],
-    directs :: [Node a] 
+    hops :: [a],
+    directs :: [a] 
 }
 
-newtype IntervalLabel a = IntervalLabel (Node a)
+
+data Parent a = NoParent | Parent a
+
+data IntervalLabel a = IntervalLabel {
+    id   :: a,
+    parent :: Parent a
+}
+
+charGraph :: Graph Char
+charGraph = Graph [ Node 'A' ['B']
+                  , Node 'B' []
+                  ]
+-- record-dotprocessor 
+-- > findPreAndPost charGraph
+--    [(A, 1, 4), (B, 2, 3)]
+--
+findPreAndPost :: forall a. Graph a -> [(a, Int, Int)]
+findPreAndPost (Graph graph) = [] --[(id (head graph :: Node a), 0, 0)]
+
+findHops :: Graph a -> [(a, [a])]
+findHops graph = undefined
+
+
+main = do
+ print $ findPreAndPost charGraph
