ff9de304062321f0a3479cb59e90c413bc3f3b4c
m-nagacharan <hello@nagacharan.me>
2021-05-20 11:58:31 +0200

    some

--- DAILabel/app/DAILabel.hs
+++ DAILabel/app/DAILabel.hs
@@ -37,6 +37,28 @@ data Labels = Labels {
     directs :: Directs
 } deriving (Typeable, Data)
 
+graph11 :: Graph
+graph11 = 
+    [ (Nd 'a', [ Nd 'b', Nd 'c'] ),
+      (Nd 'b', [ Nd 'c'] )
+    ]
+
+graph2 :: Graph
+graph2 = 
+    [ (Nd 'a', [ Nd 'b', Nd 'c'] ),
+      (Nd 'b', [ Nd 'd', Nd 'e', Nd 'f' ] ),
+      (Nd 'c', [ Nd 'h' ] ),
+      (Nd 'd', [ Nd 'k' ] ),
+      (Nd 'e', [ Nd 'g', Nd 'h' ] ),
+      (Nd 'f', [ Nd 'g' ] ),
+      (Nd 'g', [ Nd 'i', Nd 'j' ] ),
+      (Nd 'h', [ Nd 'k' ] ),
+      (Nd 'i', [ Nd 'k' ] ),
+      (Nd 'j', [ Nd 'k' ] ),
+      (Nd 'k', [] )
+    ]
+
+
 
 instance Show Nd where
   show (Nd a) = show a 
@@ -47,10 +69,18 @@ instance Show Labels where
 
 graph1 :: Table (Nd,Labels)
 graph1 = table "graph1"
-           `withIndex` node_name
+           `withIndex` graph_index
 
-node_name :: Index (Nd, Labels) Nd
-node_name = index graph1 "node_index" fst
+graph_index :: Index (Nd, Labels) Nd
+graph_index = index graph1 "node_index" fst
+
+
+counters :: Table (String, Int)
+counters = table "counter" 
+             `withIndex` counter_index
+
+counter_index :: Index (String, Int) String
+counter_index = index counters "counter_index" fst
 
 
 main :: IO ()
@@ -58,11 +88,62 @@ main = do
   db <- openDB "test1.db"
   x  <- runDaison db ReadWriteMode $ do
     tryCreateTable graph1
-    insert graph1 (return ( Nd 'a', Labels (Nd 'a') 1 10 Set.empty Set.empty ))
+    tryCreateTable counters
+    insert counters (return ( "counter", 143 ))
+{-     insert graph1 (return ( Nd 'a', Labels (Nd 'a') 1 10 Set.empty Set.empty ))
     insert graph1 (return ( Nd 'b', Labels (Nd 'a') 2 9 Set.empty Set.empty ))
-    --update people2 (\_ (name,age,x) -> (name,age,10))
+ -}    --update people2 (\_ (name,age,x) -> (name,age,10))
       --             (from people2)
+    insertT
     select [ x | x <- from graph1 everything ]
+--    head $ select [ x | x <- from counters (at 1) ]
   print x
   closeDB db
+
+
+{- getCounter :: Daison Int
+getCounter =  do
+  p <- select [ x | x <- from counters (at 1) ]
+  let m = head p
+  let n = snd m 
+  return n
+ -}
+
+
+getCounter :: Daison Int
+getCounter =  do
+  p <- select [ x | x <- from counters (at 1) ]
+  return (snd . head . p )
+
+
+
+
+
+
+incrementCounter :: Daison Int
+incrementCounter = undefined
+
+resetCounter :: Daison Int
+resetCounter = undefined
+
+
+
+
+process :: Graph -> Daison ()
+process graph = processNodes graph (Nd 'a') (Nd 'a')
+
+
+processNodes :: Graph -> Nd -> Nd -> Daison()
+processNodes graph nd parent = undefined
+
+
+--updatePost :: Graph -> Nd -> Daison ()
+
+
+
+insertT :: Daison (Key (Nd, Labels), Key (Nd, Labels))
+insertT = do
+  insert graph1 (return ( Nd 'a', Labels (Nd 'a') 1 10 Set.empty Set.empty ))
+  insert graph1 (return ( Nd 'b', Labels (Nd 'a') 2 9 Set.empty Set.empty ))
+
 
