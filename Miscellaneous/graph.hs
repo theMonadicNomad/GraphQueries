f7bf9d98d842aa0252c31dbd8c9483093e91666a
m-nagacharan <hello@nagacharan.me>
2021-09-22 12:21:26 +0200

    generating tree graph

--- Miscellaneous/graph.hs
+++ Miscellaneous/graph.hs
@@ -6,7 +6,7 @@ import Data.Int
 import Data.List
 import Data.Maybe
 import Control.Monad
-
+import System.Random.SplitMix
 type Nd = Int64
 --  deriving (Eq, Ord, Read, Data)
 type Ndc = Char
@@ -89,7 +89,7 @@ ranValues gen n a b = do
   return (values, newGen)
 
 
-
+--take 8  $ unfoldr (Just . nextFloat) (mkSMGen 1337) :: [Float]
 
 
   --ranValues :: StdGen -> Int -> IO ()
@@ -100,3 +100,86 @@ ranValues1 n a b = do
   putStrLn $ show (values)
   let (_,newGen) = random gen :: (Double, StdGen)
   putStrLn $ show (newGen ) 
+
+generateGraph12 :: Int64 -> Float -> IO (Graph Node)
+generateGraph12 n p = do
+  gen <- initSMGen
+  (_, g) <- foldM (\(genV, vs) x -> do
+        (gup, vsup) <- restList genV x
+        return (gup, vs ++ [(I x, vsup)])) 
+    (gen, []) 
+    [1..n]
+  return $ Graph g
+    where
+        restList genV x = do
+          (vs, ugen) <- ranValues12 genV ( fromEnum $  (n-x)) --0.0 1.0
+          let vs' =
+                zipWith (\v i -> if v <= p
+                              then Just i
+                              else Nothing
+                  )
+                  vs
+                  [(x+1)..(n)]
+          return (ugen, I <$> catMaybes vs')
+
+
+
+
+--ranValues12 ::  Int ->  IO ()
+ranValues12  gen n  = do 
+--  gen <- initSMGen
+  let values = take n  $ unfoldr (Just . nextFloat) (mkSMGen 1337) :: [Float]--take n (randomRs (a,b) gen )
+  putStrLn $ show (values)
+  let (_,newGen) = nextFloat gen :: (Float, SMGen)
+  putStrLn $ show (newGen ) 
+  return (values, newGen)
+
+
+generateTreeGraph :: Int64 -> Int64 -> Int64 -> IO (Graph Node)
+generateTreeGraph n total curr = do
+  if total == 0
+    then return $ Graph []
+    else do
+      let rt = curr 
+      (lrt , ch) <- genChild (total, curr + 1 , n) rt
+      let rest = (\x -> (I x, [])) <$> ([lrt+1 .. total])
+      final <- mapM (addMoreEdges total n) (ch ++ rest)
+      return $ Graph final
+
+addMoreEdges :: Int64 -> Int64 -> (Node, [Node]) -> IO (Node, [Node])
+addMoreEdges total n (I x, []) =
+  if x == total
+    then return (I x , [])
+    else do
+      gen <- newStdGen
+      ls <- mapM (\_ -> do
+        gn <- newStdGen
+        return $ fst (randomR (x + 1, total) gn)) [1..(fst $ randomR (0, n) gen)]
+      return (I x, I <$> (sort $ nub ls))
+addMoreEdges total n (I x, ls) = do
+  let I ns = last ls
+  gen <- newStdGen
+  ls' <- mapM (\_ -> do
+    gn <- newStdGen
+    return $ fst (randomR (ns + 1, total) gn)) [1..(fst $ randomR (0, n) gen)]
+  return (I x, ls ++ (I <$> (sort ls')))
+
+genChild :: (Int64, Int64, Int64) -> Int64 -> IO (Int64, [(Node, [Node])])
+genChild (total, curr, n) rt = do
+  children <- generateChildren n curr
+  let curr' = if null children && curr - 1 == rt
+                then curr + 1
+                else curr + (fromIntegral (length children ))
+  (rt',ch) <-
+    if total <= curr'
+      then return (rt, [])
+      else do
+        genChild (total, curr', n) (rt+1) 
+  return (rt', (I rt,children) : ch)
+
+generateChildren :: Int64 -> Int64 -> IO [Node]
+generateChildren n c =   do
+  gen <- newStdGen
+  let values = fst $ (randomR (1,n) gen )
+  -- putStrLn $ show (values)
+  return (I <$> [c .. (c + values - 1)] )
